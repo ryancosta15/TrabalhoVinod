1c4f319123385f56b6c12baf2876fdb984757f94
jesga06 <153961975+jesga06@users.noreply.github.com>
2025-04-03 09:18:58 -0300

    função chamada ao detectar "variável" alterada

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import monlib
 nlinha = 0 #esse nlinha é pra debug
-var = {} #dicionário pras variaveis
-script = open("codteste.txt").readlines()
+#nvar = {} #nome das variaveis
+#tvar = {} #tipo das variaveis (int, float, str e bool
+#dvar = {} #dado das variaveis (texto/valor)
+var = {}
+script = open("teste monarca.txt").readlines()
 for linha in script:
     nlinha += 1
     print(f"DEBUG - linha encontrada, buscando comandos na linha {nlinha}")
@@ -10,11 +13,8 @@ for linha in script:
         monlib.escrever(linha)
     elif "variavel " in linha:
         print("DEBUG - 'variavel' encontrada")
-        monlib.variavel(var, linha)
-    elif "limpar linha: " in linha:
-        monlib.limpar(linha)
-
-
-print(float(var["b"])+float(var["c"])) #print de debug pra ver se tava salvando as variáveis certinho
+        monlib.variaveltipo(linha, var)
+#print(float(var["b"])+float(var["c"]))
+# #print de debug pra ver se tava salvando as variáveis certinho
 
 
