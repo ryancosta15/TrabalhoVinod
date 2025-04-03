8810c11e34459a6d7015d426c272bd9e0f2c80a3
jesga06 <153961975+jesga06@users.noreply.github.com>
2025-04-03 13:41:46 -0300

    função para adicionar variáveis modificada

--- monlib.py
+++ monlib.py
@@ -1,10 +1,11 @@
 def escrever(linha):
     linha = linha.replace("escrever: ", "")
     print(linha)
-def variavel(var, linha): #func pra definir variaveis
-    linha = linha.replace("variavel ", "", ).replace(":", "").split(" ")
-    var.update({linha[0]: linha[1]})
+#def variavel(var, linha): #func pra definir variaveis
+#    linha = linha.replace("variavel ", "", ).replace(":", "").split(" ")
+#    var.update({linha[0]: linha[1]})
 def variaveltipo(linha, var):
-    linha = linha.replace("variavel ", "", ).replace(":", "").split(" ")
-    linh = linha.pop(1)  # se eu escrever "linha" o comando da um erro, se eu manter "linh" ele funciona. computadores me confudem muito
-    var.update({linha[0]: linha[1], linha[2]:0,})
+    nome = linha.split(' ')[1]
+    tipo = linha.split(' ')[3][0:-1]
+    dado = ' '.join(linha.split(' ')[4:])
+    var.update({nome: [tipo, dado]})
